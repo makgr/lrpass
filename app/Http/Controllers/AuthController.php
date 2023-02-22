b9b9eb1d9acbd37c235b193bb1d78e566338e27a
Mohammad Ali Khan <xvirus.bd@gmail.com>
2023-02-22 23:32:34 +0600

    register request created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use DB;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegiserRequest;
 
 class AuthController extends Controller
 {
-    //
+    public function Login(Request $request)
+    {
+
+        try {
+
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('app')->accessToken;
+
+                return response([
+                    'message' => "Successfully Login",
+                    'token' => $token,
+                    'user' => $user
+                ], 200); // States Code
+            }
+        } catch (\Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }
+        return response([
+            'message' => 'Invalid Email Or Password'
+        ], 401);
+    }
+
+    public function Register(RegiserRequest $request)
+    {
+
+        try {
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            $token = $user->createToken('app')->accessToken;
+
+            return response([
+                'message' => "Registration Successfull",
+                'token' => $token,
+                'user' => $user
+            ], 200);
+        } catch (\Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgetController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Login Routes 
+Route::post('/login', [AuthController::class, 'Login']);
+// Register Routes 
+Route::post('/register', [AuthController::class, 'Register']);
+
+// Forget Password Routes 
+Route::post('/forgetpassword', [ForgetController::class, 'ForgetPassword']);
+
+// Reset Password Routes 
+Route::post('/resetpassword', [ResetController::class, 'ResetPassword']);
+
+// Current User Route 
+Route::get('/user', [UserController::class, 'User'])->middleware('auth:api');
